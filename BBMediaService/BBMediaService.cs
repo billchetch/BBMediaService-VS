4e91335506339f607d528fe111c98a6d29c96d2b
billchetch <bill@chetch.net>
2019-12-23 23:57:12 +0800

    Add project files.

---
+++ BBMediaService/BBMediaService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BBMediaService
+{
+    public partial class BBMediaService : ServiceBase
+    {
+        public BBMediaService()
+        {
+            InitializeComponent();
+            eventLog.Source = "BBMediaService Class";
+            eventLog.Log = "BB Media Service";
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            try
+            {
+                eventLog.WriteEntry("Serivce started", EventLogEntryType.Information);
+            } catch (Exception e)
+            {
+                eventLog.WriteEntry(e.Message, EventLogEntryType.Error);
+            }
+        }
+
+        protected override void OnStop()
+        {
+            eventLog.WriteEntry("Serivce stopped", EventLogEntryType.Information);
+        }
+    }
+}
